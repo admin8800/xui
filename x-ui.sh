0c79fc0bfac14dd92c4c6b9eba4398bccb79bd17
admin8800 <153331261+admin8800@users.noreply.github.com>
2024-06-28 06:47:11 +0800

    Update x-ui.sh

--- x-ui.sh
+++ x-ui.sh
@@ -416,7 +416,7 @@ show_xray_status() {
 }
 
 ssl_cert_issue() {
-    wget -N --no-check-certificate https://raw.githubusercontent.com/Misaka-blog/acme-script/main/acme.sh && bash acme.sh
+    wget -N --no-check-certificate https://github.com/admin8800/shell/raw/main/acme.sh && bash acme.sh
 }
 
 open_ports(){
