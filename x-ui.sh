1d37b67816edc1450cbff4f35315edb872b526af
admin8800 <153331261+admin8800@users.noreply.github.com>
2024-07-01 05:08:02 +0800

    修改

--- x-ui.sh
+++ x-ui.sh
@@ -106,7 +106,7 @@ before_show_menu() {
 }
 
 install() {
-    bash <(curl -Ls https://raw.githubusercontent.com/admin8800/x-ui/master/install.sh)
+    bash <(curl -Ls https://raw.githubusercontent.com/admin8800/xui/main/install.sh)
     if [[ $? == 0 ]]; then
         if [[ $# == 0 ]]; then
             start
@@ -125,7 +125,7 @@ update() {
         fi
         return 0
     fi
-    bash <(curl -Ls https://raw.githubusercontent.com/admin8800/x-ui/master/install.sh)
+    bash <(curl -Ls https://raw.githubusercontent.com/admin8800/xui/main/install.sh)
     if [[ $? == 0 ]]; then
         LOGI "更新完成，已自动重启面板 "
         exit 0
@@ -309,7 +309,7 @@ install_bbr() {
 }
 
 update_shell() {
-    wget -O /usr/bin/x-ui -N --no-check-certificate https://github.com/admin8800/x-ui/raw/master/x-ui_CN.sh
+    wget -O /usr/bin/x-ui -N --no-check-certificate https://github.com/admin8800/xui/raw/main/x-ui.sh
     if [[ $? != 0 ]]; then
         echo ""
         LOGE "下载脚本失败，请检查本机能否连接 Github"
