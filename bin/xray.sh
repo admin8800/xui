c50ba4d39097ed5764c17a49944c59fbaffff13e
admin8800 <153331261+admin8800@users.noreply.github.com>
2024-07-19 18:26:45 +0800

    Update xray.sh

--- bin/xray.sh
+++ bin/xray.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# 获取脚本所在目录
 SCRIPT_DIR=$(dirname "$0")
 
-# 切换到脚本所在目录
 cd "$SCRIPT_DIR"
 
 # 下载最新的 geoip.dat 和 geosite.dat 文件
@@ -17,21 +15,18 @@ download_and_extract() {
     local url="https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-$arch.zip"
     local temp_dir=$(mktemp -d)
     
-    # 下载 zip 文件到临时目录
     wget -q -O "$temp_dir/xray.zip" "$url"
 
-    # 解压到临时目录
     unzip -q "$temp_dir/xray.zip" -d "$temp_dir"
 
-    # 移动并重命名核心文件到脚本目录
     mv "$temp_dir/xray" "$SCRIPT_DIR/$newname"
 
-    # 清理临时目录
     rm -rf "$temp_dir"
 }
 
-# 下载并解压不同架构的 xray 核心，并进行重命名
 download_and_extract "64" "xray-linux-amd64"
 download_and_extract "s390x" "xray-linux-s390x"
 download_and_extract "arm64-v8a" "xray-linux-arm64"
 download_and_extract "arm32-v7a" "xray-linux-arm32"
+
+echo "xray内核更新完成。"
